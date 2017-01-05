b1d447e3d8ad5174b22430ec851cb79e9ac5fee6
Kazuhiko Arase <kazuhiko.arase@gmail.com>
2017-01-05 16:46:28 +0900

    Nashorn support

--- src/main/webapp/test.js
+++ src/main/webapp/test.js
@@ -1,4 +1,11 @@
 $(function() {
+  
+  var mobile = !!navigator.userAgent.match(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i);
+  if (mobile) {
+    $('HEAD').prepend('<meta name="viewport" content="width=device-width,' +
+      'initial-scale=1, minimum-scale=1, maximum-scale=1" />');
+  }
+
   var buildUrl = function(path) {
     var url = location.href;
     var i1 = url.indexOf(':');
