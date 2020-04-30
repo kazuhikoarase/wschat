63faae913665f2b8b3e2191d1ebc4b8bb6e1709a
Kazuhiko Arase <kazuhiko.arase@gmail.com>
2020-05-01 02:35:03 +0900

    commit

--- src/main/java/wschat/impl/UserService.java
+++ src/main/java/wschat/impl/UserService.java
@@ -1,9 +1,8 @@
 package wschat.impl;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 import wschat.IUserService;
 
@@ -13,15 +12,16 @@ import wschat.IUserService;
  */
 public class UserService implements IUserService {
 
-  private final Map<String,Map<String,String>> sessionMap =
-      new HashMap<String,Map<String,String>>();
+  private final ConcurrentHashMap<String,ConcurrentHashMap<String,String>>
+    sessionMap =
+      new ConcurrentHashMap<String,ConcurrentHashMap<String,String>>();
 
   public UserService() {
   }
 
   @Override
   public String getUserSession(String uid, String sid) {
-    Map<String,String> sessions = sessionMap.get(uid);
+    ConcurrentHashMap<String,String> sessions = sessionMap.get(uid);
     if (sessions == null) {
       return null;
     }
@@ -29,16 +29,16 @@ public class UserService implements IUserService {
   }
   @Override
   public void putUserSession(String uid, String sid, String jsonData) {
-    Map<String,String> sessions = sessionMap.get(uid);
+    ConcurrentHashMap<String,String> sessions = sessionMap.get(uid);
     if (sessions == null) {
-      sessions = new HashMap<String,String>();
+      sessions = new ConcurrentHashMap<String,String>();
       sessionMap.put(uid, sessions);
     }
     sessions.put(sid, jsonData);
   }
   @Override
   public void removeUserSession(String uid, String sid) {
-    Map<String,String> sessions = sessionMap.get(uid);
+    ConcurrentHashMap<String,String> sessions = sessionMap.get(uid);
     if (sessions == null) {
       return;
     }
@@ -47,10 +47,10 @@ public class UserService implements IUserService {
   @Override
   public List<String> getUserSessionIdList(String uid) {
     List<String> sidList = new ArrayList<String>();
-    Map<String,String> sessions = sessionMap.get(uid);
+    ConcurrentHashMap<String,String> sessions = sessionMap.get(uid);
     if (sessions != null) {
       sidList.addAll(sessions.keySet() );
-    }    
+    }
     return sidList;
   }
 }
